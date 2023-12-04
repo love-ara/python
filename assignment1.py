ae519d0f93c62b705394dcef0169c404f9ac2aeb
love-ara <aramideotenaike@gmail.com>
2023-12-04 01:20:05 +0100

    assignments

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,7 @@
 odd_numbers = 0
 even_numbers = 0
 
-for number in range(1, 100):
+for number in range(1, 101):
 	if number%2 == 0:
 		even_numbers += 1
 	if number%2 !=0: 
---
+++ number_raised.py
@@ -0,0 +1,8 @@
+number = float(input("Enter the base: "))
+number1 = float(input("Enter the power: "))
+
+#count = 1
+
+#if count <= number1:
+result = number**number1
+print(result)
